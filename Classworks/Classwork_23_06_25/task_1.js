136cf07fc92058741a4a4b3a1ec92ea5c3762549
mehebbetfz <me.farzaliyev@gmail.com>
2025-06-23 15:56:30 +0400

    Classwork_23_06_25

--- Classworks/Classwork_23_06_25/task_1.js
+++ Classworks/Classwork_23_06_25/task_1.js
@@ -1,5 +1,16 @@
-function summa(arr) {
+function summa1(arr) {
 	return arr.reduce((total, num) => total + num);
 }
 
-console.log(summa([1, 2, 3, 4, 5]));
+console.log(summa1([1, 2, 3, 4, 5]));
+
+
+function summa2(arr) {
+	let total = 0;
+	for (let i = 0; i < arr.length; i++) {
+		total += arr[i];
+	}
+	return total;
+}
+
+console.log(summa2([1, 2, 3, 4, 5]));
