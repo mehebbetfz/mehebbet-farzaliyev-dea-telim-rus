5fd6c579a6ffd07427aa62c60bbe33e857a84b71
mehebbetfz <me.farzaliyev@gmail.com>
2025-06-26 15:33:22 +0400

    Classwork_23_06_25

--- Classworks/Classwork_23_06_25/task_2.js
+++ Classworks/Classwork_23_06_25/task_2.js
@@ -1,9 +1,10 @@
 function treeWithStars() {
 		let tree = '';
 		const height = 5;
+		const width = 8;
 		
 		for (let i = 0; i < height; i++) {
-				const spaces = ' '.repeat(height - i - 1);
+				const spaces = ' '.repeat(width - i - 1);
 				const stars = '*'.repeat(2 * i + 1);
 				tree += spaces + stars + spaces + '\n';
 		}
@@ -12,3 +13,10 @@ function treeWithStars() {
 }
 
 console.log(treeWithStars());
+
+
+function sumOfPositiveNegative(arr) {
+	return arr.flat().filter((num) => num < 0).reduce((total, num) => total + num, 0);
+}
+
+console.log(sumOfPositiveNegative([[1, 2, -3, -4, 5], [3, 4, 5, 6, -7], [5, 6, 7, 8, -9]]));
---
+++ Classworks/Classwork_26_06_25/task_1.js
@@ -0,0 +1,10 @@
+function sumNumbersBetweenStartAndEnd() {
+		const start = 1;
+		const end = 100;
+
+		return ((end - start + 1) * (start + end)) / 2;
+
+}
+
+
+console.log(sumNumbersBetweenStartAndEnd());
