3b2df6f33386bd665fbf3f9588b8132cc0d0c9f8
mehebbetfz <me.farzaliyev@gmail.com>
2025-06-26 16:53:43 +0400

    Classwork_26_06_25

--- Classworks/Classwork_26_06_25/task_4.js
+++ Classworks/Classwork_26_06_25/task_4.js
@@ -1,4 +1,4 @@
-function findStartEndIndexes(arr, target) {
+function findStartEndIndexes1(arr, target) {
 	const result = [];
 	
 	for (let num of arr) {
@@ -13,4 +13,19 @@ function findStartEndIndexes(arr, target) {
 	return result;
 }
 
-console.log(findStartEndIndexes([1, 2, 3, 3, 3, 4, 5], 3));
+console.log(findStartEndIndexes1([1, 2, 3, 3, 3, 4, 5], 3));
+
+
+function findStartEndIndexes2(arr, target) {
+	const start = arr.indexOf(target);
+	if (start === -1) return [];
+	
+	let end = start;
+	while (end + 1 < arr.length && arr[end + 1] === target) {
+		end++;
+	}
+	
+	return [start, end + 1];
+}
+
+console.log(findStartEndIndexes2([1, 2, 3, 3, 3, 4, 5], 3))
