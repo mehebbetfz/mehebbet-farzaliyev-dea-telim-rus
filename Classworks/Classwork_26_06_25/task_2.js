e8e387639c2b14ca7b6e6e5a7b25d9ac3994cb08
mehebbetfz <me.farzaliyev@gmail.com>
2025-06-26 15:44:34 +0400

    Classwork_26_06_25

---
+++ Classworks/Classwork_26_06_25/task_2.js
@@ -0,0 +1,18 @@
+function fizzBuzz(n) {
+	let result = "";
+	
+	for (let i = 0; i < n; i++) {
+		if (i % 3 === 0 && n % 5 === 0) {
+			result = "FizzBuzz";
+		}else 		if (i % 3 === 0 ) {
+			result = "Fizz";
+		} else 		if (n % 5 === 0) {
+			result = "Buzz";
+		}
+	}
+	
+	return result;
+}
+
+console.log(fizzBuzz(56))
+
