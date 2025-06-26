f06696545aab5d36ab20400399b24b1cb9940a58
mehebbetfz <me.farzaliyev@gmail.com>
2025-06-26 16:19:25 +0400

    Classwork_26_06_25

--- Classworks/Classwork_26_06_25/task_2.js
+++ Classworks/Classwork_26_06_25/task_2.js
@@ -1,17 +1,17 @@
 function fizzBuzz(n) {
-	let result = "";
+	let result = ''
 	
 	for (let i = 0; i < n; i++) {
 		if (i % 3 === 0 && n % 5 === 0) {
-			result = "FizzBuzz";
-		}else 		if (i % 3 === 0 ) {
-			result = "Fizz";
-		} else 		if (n % 5 === 0) {
-			result = "Buzz";
+			result = 'FizzBuzz'
+		} else if (i % 3 === 0) {
+			result = 'Fizz'
+		} else if (n % 5 === 0) {
+			result = 'Buzz'
 		}
 	}
 	
-	return result;
+	return result
 }
 
 console.log(fizzBuzz(56))
---
+++ Classworks/Classwork_26_06_25/task_3.js
@@ -0,0 +1,16 @@
+function fibonachi(n) {
+	let a = 0;
+	let b = 1;
+	let result = [];
+	
+	for (let i = 0; i < n; i++) {
+		result.push(a);
+		let temp = a;
+		a = b;
+		b = temp + b;
+	}
+	
+	return result;
+}
+
+console.log(fibonachi(10))
